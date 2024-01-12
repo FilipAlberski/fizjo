24c2a51af970264c96e4603e31aecfcb7178a79c
Filip Alberski <alberskif@gmail.com>
2024-01-12 23:18:10 +0100

    Refactor Alert component to add optional delay and fix hover behavior

--- client/src/components/Alert.tsx
+++ client/src/components/Alert.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeAlert } from '../redux/slices/alertSlice';
+
+//TODO: nie dziala hover na alertach, trzymanie myszy na alercie nie sprawia ze sie nie zamyka
+//TODO: dodac linear loading bar na dole kazdego alertu ktory sie zamyka po 5 sekundach
 
 interface IAlertState {
   alert: {
@@ -8,6 +11,7 @@ interface IAlertState {
       id: number;
       message: string;
       type: string;
+      delay?: number; // Make delay optional
     }[];
   };
 }
@@ -17,22 +21,37 @@ const Alert = () => {
   const alerts = useSelector(
     (state: IAlertState) => state.alert.alerts
   );
-  const [hovered, setHovered] = useState(false);
+  const alertTimeouts = useRef<{ [key: number]: NodeJS.Timeout }>({});
 
-  const removeAlertAfterDelay = (alertId: number, delay: number) => {
-    setTimeout(() => {
-      if (!hovered) {
-        dispatch(removeAlert(alertId));
-      }
+  const removeAlertWithDelay = (
+    alertId: number,
+    delay: number = 5000
+  ) => {
+    // Default delay of 5000
+    clearTimeout(alertTimeouts.current[alertId]); // Clear existing timeout
+    alertTimeouts.current[alertId] = setTimeout(() => {
+      dispatch(removeAlert(alertId));
     }, delay);
   };
 
   useEffect(() => {
     alerts.forEach((alert) => {
-      removeAlertAfterDelay(alert.id, 5000); // 5 seconds delay
+      removeAlertWithDelay(alert.id, alert.delay); // Use the alert's delay or default
     });
-    console.log(alerts);
-  }, [alerts, hovered, dispatch]);
+
+    // Cleanup timeouts on unmount
+    return () => {
+      Object.values(alertTimeouts.current).forEach(clearTimeout);
+    };
+  }, [alerts, dispatch]);
+
+  const handleMouseEnter = (alertId: number) => {
+    clearTimeout(alertTimeouts.current[alertId]); // Pause timeout on hover
+  };
+
+  const handleMouseLeave = (alertId: number, delay?: number) => {
+    removeAlertWithDelay(alertId, delay); // Restart timeout on mouse leave
+  };
 
   return (
     <div className="absolute w-80 top-0 left-0 m-4 z-50 flex flex-col gap-3 max-h-screen overflow-hidden">
@@ -40,10 +59,16 @@ const Alert = () => {
         <div
           key={alert.id}
           className={`alert alert-${alert.type}`}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
+          onMouseEnter={() => handleMouseEnter(alert.id)}
+          onMouseLeave={() => handleMouseLeave(alert.id, alert.delay)}
         >
           <p>{alert.message}</p>
+          <button
+            onClick={() => dispatch(removeAlert(alert.id))}
+            className="close-btn"
+          >
+            Close
+          </button>
         </div>
       ))}
     </div>
