3060735258eb56403d1090fd4d02948de8e6898e
Filip Alberski <alberskif@gmail.com>
2024-01-29 20:17:46 +0100

    test

---
+++ server/src/config/mailTemplates.ts
@@ -0,0 +1,11 @@
+const mailTemplates = {
+  confirmEmail: {
+    subject: 'Confirm your email',
+    template: 'confirmEmail',
+  },
+  resetPassword: {
+    subject: 'Reset your password',
+    template: 'resetPassword',
+  },
+};
+export default mailTemplates;
