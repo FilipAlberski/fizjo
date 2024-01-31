78a379b01bca821752d62fbc44febc8e8b66e024
Filip Alberski <alberskif@gmail.com>
2024-01-31 19:53:53 +0100

    Add token refresh functionality to baseQueryWithReauth

--- client/src/redux/slices/api/apiSlice.ts
+++ client/src/redux/slices/api/apiSlice.ts
@@ -15,8 +15,21 @@ const baseQuery = fetchBaseQuery({
   credentials: 'include',
 });
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error?.status === 401) {
+    console.log('401 error, trying to refresh token');
+    const refreshResult = await api.endpoints.refresh();
+    if (refreshResult.error) {
+      return refreshResult;
+    }
+    result = await baseQuery(args, api, extraOptions);
+  }
+};
+
 export const apiSlice = createApi({
   baseQuery: baseQuery,
+  tagTypes: ['User'],
 
   endpoints: (builder) => ({}),
 });
