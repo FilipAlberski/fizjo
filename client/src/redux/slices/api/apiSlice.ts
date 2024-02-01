11385a87c54a0c354c6e05c4f2933b07c21dae84
Filip Alberski <alberskif@gmail.com>
2024-02-01 21:27:21 +0100

    typescript for apislice

--- client/src/redux/slices/api/apiSlice.ts
+++ client/src/redux/slices/api/apiSlice.ts
@@ -1,11 +1,24 @@
 import {
   createApi,
   fetchBaseQuery,
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
 } from '@reduxjs/toolkit/query/react';
+
+interface AuthState {
+  token: string | null;
+}
+
+interface AppState {
+  auth: AuthState;
+}
+
+type GetState = () => AppState;
 
 const baseQuery = fetchBaseQuery({
   baseUrl: 'http://localhost:8080/api/v1',
-  prepareHeaders: (headers, { getState }) => {
+  prepareHeaders: (headers, { getState }: { getState: GetState }) => {
     const token = getState().auth.token;
     if (token) {
       headers.set('authorization', `Bearer ${token}`);
@@ -15,20 +28,27 @@ const baseQuery = fetchBaseQuery({
   credentials: 'include',
 });
 
-const baseQueryWithReauth = async (args, api, extraOptions) => {
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
   if (result.error?.status === 401) {
     console.log('401 error, trying to refresh token');
-    const refreshResult = await api.endpoints.refresh();
+    const refreshResult = await api.endpoint.refresh.initiate(null, {
+      forceRefetch: true,
+    });
     if (refreshResult.error) {
       return refreshResult;
     }
     result = await baseQuery(args, api, extraOptions);
   }
+  return result;
 };
 
 export const apiSlice = createApi({
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['User'],
 
   endpoints: (builder) => ({}),
